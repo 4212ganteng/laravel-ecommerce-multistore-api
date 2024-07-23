88049961e6e2055a114f54d5f4cd2c4582777cec
4212ganteng <azizmuslim051@gmail.com>
2024-07-23 09:00:19 +0700

    fix migration category

--- database/migrations/2024_07_23_014618_create_categories_table.php
+++ database/migrations/2024_07_23_014618_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             // seller_id
-            $table->foreignId('seller_id')->constrained()->onDelete('cascade');
+            $table->foreignId('seller_id')->constrained('users')->onDelete('cascade');
             $table->string('name');
             // description
             $table->text('description')->nullable();
