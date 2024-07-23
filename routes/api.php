71fa21b7d62c3df40958435e0630eba0ae4a1d76
4212ganteng <azizmuslim051@gmail.com>
2024-07-23 08:55:59 +0700

    cartegory

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,15 @@ Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login'])
 // logout
 Route::post('/logout', [App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+
 // register seller
 Route::post('/seller/register', [App\Http\Controllers\Api\AuthController::class, 'registerSeller']);
 // register customer
-Route::post('/customer/register', [App\Http\Controllers\Api\AuthController::class, 'registerCustomer']);
+Route::post('/customer/register', [App\Http\Controllers\Api\AuthController::class, 'registerUser']);
 
 
+// store category
+Route::post('seller/category', [App\Http\Controllers\Api\CategoryController::class, 'store'])->middleware('auth:sanctum');
+// get all categories
+Route::get('seller/category', [App\Http\Controllers\Api\CategoryController::class, 'index'])->middleware('auth:sanctum');
 
